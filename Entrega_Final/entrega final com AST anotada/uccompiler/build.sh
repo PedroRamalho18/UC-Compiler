1ee5b1162feb529d0474b79bd454800cca054277
Pedro Ramalho <pedroptramalho@gmail.com>
2024-02-11 20:36:06 +0000

    final

--- Entrega_Final/entrega final com AST anotada/uccompiler/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f uccompiler lex.yy.c y.tab.c y.tab.h
-yacc -d -v -t -g -Wcounterexamples --report=all uccompiler.y 
-lex uccompiler.l
-cc -o uccompiler lex.yy.c y.tab.c ast.c semantics.c -g -Wall -Wno-unused-function
